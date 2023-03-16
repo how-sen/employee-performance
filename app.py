6b1b378d62ba7183cf3252663e222d52aa08d107
Haosen Xu <jose>
2023-03-16 14:17:09 -0500

    renamed front-end to frontend

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ from api.routes import api_bp
 from api.database import db
 from api.modules.view import views_bp
 
-app = Flask(__name__, static_url_path='', static_folder='front-end/build')
-@app.route("/", defaults={'path':''})
-def serve(path):
-    return send_from_directory(app.static_folder,'index.html')
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 # CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///staff_performance.db'
 db.init_app(app)
 
 app.register_blueprint(api_bp)
 app.register_blueprint(views_bp)
+
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
 
