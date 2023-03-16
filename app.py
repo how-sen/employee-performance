ac021c8f5a93e1707f55b21d73b892c729f54d7d
Haosen Xu <jose>
2023-03-16 14:49:44 -0500

    try update the app.py again

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ from api.routes import api_bp
 from api.database import db
 from api.modules.view import views_bp
 
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+app = Flask(__name__)
 # CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///staff_performance.db'
-db.init_app(app)
+app.static_folder = 'frontend/build'
 
+db.init_app(app)
 app.register_blueprint(api_bp)
 app.register_blueprint(views_bp)
 
-@app.route("/", defaults={'path':''})
+@app.route("/", defaults={'path': ''})
 def serve(path):
-    return send_from_directory(app.static_folder,'index.html')
+    return send_from_directory(app.static_folder, 'index.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
